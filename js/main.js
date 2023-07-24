aa4fbe5d9771c23efab3b2174385c56757e9bb53
agha2023 <mohamedmahmoudagha2023@gmail.com>
2023-07-24 17:24:58 +0200

    sec commit

--- js/main.js
+++ js/main.js
@@ -10,13 +10,7 @@ panels.forEach((panel) => {
 
 
 function removeActiveClasses() {
-  panels.forEach( functionecho "# Accordion-Style" >> README.md
-  git init
-  git add README.md
-  git commit -m "first commit"
-  git branch -M main
-  git remote add origin https://github.com/agha2023/Accordion-Style.git
-  git push -u origin main(panel){
+  panels.forEach( function(panel){
     panel.classList.remove('active');
   });
 }
